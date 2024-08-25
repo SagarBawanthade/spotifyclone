c9691672b073c14e44889687ea4e4c7553503c21
Sagar Bawanthade <137410094+SagarBawanthade@users.noreply.github.com>
2024-08-25 15:34:24 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull sagarbawanthade/spotify
+docker pull sagarbawanthade/spotify:v1
 
 # Run the Docker image as a container
-docker run -d -p 8000:80 sagarbawanthade/spotify
+docker run -d -p 8000:80 sagarbawanthade/spotify:v1
